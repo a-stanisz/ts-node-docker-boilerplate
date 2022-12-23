2f54b7de61c35bac7efb3a52bc261763c3e42696
a-stanisz <hello@adamstaniszewski.com>
2022-12-23 12:52:56 +0100

    Add type checking

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import http from "http";
+import { createServer, IncomingMessage, ServerResponse } from 'http'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const requestListener = (___, res) => {
-  res.writeHead(200);
-  res.end("Hello!");
-};
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+  console.log(req)
+  res.writeHead(200)
+  res.end('Hello!')
+}
 
-const server = http.createServer(requestListener);
+const server = createServer(requestListener)
 
-server.listen(PORT);
+server.listen(PORT)
 
-console.log(`server listens at port ${PORT}`);
+console.log(`server listens at port ${PORT}`)
